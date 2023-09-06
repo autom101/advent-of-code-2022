f9a8a124e154515516459e4aa83ad014bb0791f4
Sameer Ojha <122489814+autom101@users.noreply.github.com>
2023-09-06 16:09:08 -0600

    Finish q1

--- 1/main.js
+++ 1/main.js
@@ -1,25 +1,28 @@
 const axios = require("axios");
-
-//Read file in, every time there is an empty space, skip over it and start adding up calories again:
-
-const getData = async (path) => {
-  const response = await axios.get(path);
-  return response.data;
-};
+const fs = require("fs");
 
 let curSum = 0;
 let maxSoFar = 0;
 
-// just pretend I get the data for now...
-const path = "temp";
-const data = getData(path); //Should be an array of some sort, maybe
+/* 
+1. Read data using the fs module's read function and convert it to a string
+2. Split the String using the delimiter "\r\n" to obtain an array of strings
+3. Iterate through the data array and convert each item to a Number before adding it to curSum, updating the max each time. 
+4. In step 3, if the item is a 0, that means the current person's calories are finished.
+5. Return the calories
+*/
+
+const data = fs.readFileSync("../data/1.txt", "utf-8").toString().split("\r\n");
 
 data.forEach((elem) => {
-  if (elem > 0) {
-    curSum += elem;
+  let num = Number(elem);
+  if (num > 0) {
+    curSum += num;
   } else {
     curSum = 0;
   }
 
   maxSoFar = Math.max(maxSoFar, curSum);
 });
+
+console.log(maxSoFar);
