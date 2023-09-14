86b890e5c48fcb02404616ba0f9eeabe028b836f
Sameer Ojha <122489814+autom101@users.noreply.github.com>
2023-09-14 10:23:37 -0600

    Finish day 5 part 1

---
+++ js/5.1.js
@@ -0,0 +1,41 @@
+const { getData } = require("./_data-extractor");
+/* 
+First fill up the stacks array with the data
+Iterate through the data and 
+*/
+
+const stackData = getData("../data/5-stacks.txt");
+const data = getData("../data/5.txt");
+
+const normal = [[" "], [], [], [], [], [], [], [], [], []];
+const test = [[""], [], [], []];
+
+const stacks = normal;
+
+let stackNum = 1;
+stackData.forEach((elem) => {
+  const elemArr = elem.split(" ");
+  elemArr.forEach((item) => {
+    stacks[stackNum].push(item);
+  });
+
+  stackNum++;
+});
+
+data.forEach((elem) => {
+  const elemArr = elem.split(" ");
+  const count = Number(elemArr[1]);
+  const sender = stacks[Number(elemArr[3])]; //remove elements from this
+  const receiver = stacks[Number(elemArr[5])]; //put elements in this
+
+  for (let i = 0; i < count; i++) {
+    const item = sender.pop();
+    receiver.push(item);
+  }
+});
+
+let ans = stacks.reduce((total, cur) => {
+  return total + cur.pop();
+}, "");
+
+console.log(ans);
