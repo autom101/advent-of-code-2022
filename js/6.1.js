4b365618b2a20238625861c4a045fe79cc105d0c
Sameer Ojha <122489814+autom101@users.noreply.github.com>
2023-09-14 12:49:55 -0600

    Finish day 6 part 1

---
+++ js/6.1.js
@@ -0,0 +1,33 @@
+const { getData } = require("./_data-extractor");
+
+const data = getData("../data/6.txt");
+
+const ansArray = [];
+data.forEach((elem) => {
+  let str = "";
+
+  // a sliding window question that looks for a set of 4 characters.
+  for (let i = 0; i < elem.length; i++) {
+    const cur = elem[i];
+    if (str.includes(cur)) {
+      //sequence is repeating but is not of length 4? reset:
+      let tmp = 0;
+      while (str[tmp] !== cur) {
+        tmp++;
+      }
+      str = str.slice(tmp + 1);
+    }
+
+    str += cur;
+
+    if (str.length == 4) {
+      ansArray.push(i + 1);
+      break;
+    }
+  }
+  console.log(str);
+});
+
+ansArray.forEach((ans) => {
+  console.log(ans);
+});
